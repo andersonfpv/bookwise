9e1a2b91cf7dc9e4561e562238ea04415fc7aec9
A12 <A12@NOTEA1208.santuario.local>
2025-05-31 20:18:59 -0300

    criando o card do livro

--- index.php
+++ index.php
@@ -26,9 +26,21 @@
             <button type="submit">🔎</button>
         </form>
 
-        <div>
-            lista final
-        </div>
+        <section class="space-y-4">
+            <!-- Livro -->
+            <div class="w-1/3 p-2 rounded border-stone-800 border-2 bg-stone-900">
+                <div class="flex">
+                    <div class="w-1/3">imagem</div>
+                    <div>
+                        <div class="font-semibold">titulo</div>
+                        <div class="text-xs italic">Autor</div>
+                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 avaliações)</div>
+                    </div>
+                </div>
+                
+                <div class="text-sm">Descrição</div>
+            </div>
+        </section>
     </main>
 </body>
 </html>
