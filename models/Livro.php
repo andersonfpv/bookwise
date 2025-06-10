82dcdd8b27336aaad624bb39cdcfc81f9476ad3e
A12 <A12@NOTEA1208.santuario.local>
2025-06-10 17:13:52 -0300

    Criando o model de livro

---
+++ models/Livro.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Representação de 1 Registro do banco de dados
+ * em forma de CLASSE
+ */
+
+class Livro {
+    public $id;
+    public $titulo;
+    public $autor;
+    public $descricao;
+}
