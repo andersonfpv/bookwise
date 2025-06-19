c1fbf5684ad1eb3de8330800de25d9ab7a671ba5
A12 <A12@NOTEA1208.santuario.local>
2025-06-18 21:48:33 -0300

    Refatorando a classe DB

--- controllers/index.controller.php
+++ controllers/index.controller.php
@@ -2,6 +2,9 @@
 
 $pesquisar = $_REQUEST['pesquisar'] ?? '';
 
-$livros = (new DB)->livros($pesquisar);
+$livros = (new DB)->query(
+    query: "select * from livros where titulo like :filtro", 
+    class: Livro::class, 
+    params: ['filtro' => "%$pesquisar%"])->fetchAll();
 
 view('index', compact('livros'));
