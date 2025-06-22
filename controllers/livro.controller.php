db1555a5a3208b461b143a0360ab6bb21eb678d9
A12 <A12@NOTEA1208.santuario.local>
2025-06-22 20:30:46 -0300

    Criando o arquivo config

--- controllers/livro.controller.php
+++ controllers/livro.controller.php
@@ -1,6 +1,6 @@
 <?php
 
-$livro = (new DB)->query("select * from livros where id = :id", Livro::class, ['id' => $_GET['id']])->fetch();
+$livro = $database->query("select * from livros where id = :id", Livro::class, ['id' => $_GET['id']])->fetch();
 
 view('livro', compact('livro'));
 
--- database.php
+++ database.php
@@ -4,9 +4,12 @@ class DB {
 
     private $db;
 
-    public function __construct()
+    public function __construct($config)
     {
-        $this -> db = new PDO('sqlite:database.sqlite');
+        
+        $connectionString = $config['driver']. ':'. $config['database'];
+
+        $this -> db = new PDO($connectionString);
     }
 
     public function query($query, $class = null, $params = []){
@@ -22,3 +25,5 @@ class DB {
     }
 
 }
+
+$database = new DB($config['database']);
