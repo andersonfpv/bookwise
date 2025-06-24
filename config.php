93339c4652940d9e48498a1547f9e9c3668d6800
A12 <A12@NOTEA1208.santuario.local>
2025-06-23 21:51:09 -0300

    Criando conexão com o mysql

--- config.php
+++ config.php
@@ -4,5 +4,14 @@ return [
     'database' => [
         'driver' => 'sqlite',
         'database' => 'database.sqlite'   
+
+       
+            // 'driver' => 'mysql',
+            // 'host' => 'localhost',
+            // 'port' => 3306,
+            // 'dbname' => 'bookwise',
+            // 'user' => 'root',
+            // 'charset' => 'utf8mb4'
+   
     ],
 ];
