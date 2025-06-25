e1af51af8a2c95da7576cfe2de0d7d7e0d002fa9
A12 <A12@NOTEA1208.santuario.local>
2025-06-24 21:33:54 -0300

    Métodos HTTP

--- views/login.view.php
+++ views/login.view.php
@@ -4,7 +4,7 @@
 
         <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Login</h1>
 
-        <form class="p-4 space-y-4">
+        <form class="p-4 space-y-4" method="POST">
 
             <div class="flex flex-col">
 
@@ -32,7 +32,7 @@
 
         <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Registro</h1>
 
-        <form class="p-4 space-y-4">
+        <form class="p-4 space-y-4" method="POST" action="/registrar">
 
             <div class="flex flex-col">
 
