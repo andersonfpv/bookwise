98c6ac104632198bf1f599be7011b6546cc185f9
A12 <A12@NOTEA1208.santuario.local>
2025-07-04 23:59:45 -0300

    Finalizando o projeto

--- views/index.view.php
+++ views/index.view.php
@@ -1,7 +1,9 @@
 <form class="w-full flex space-x-2 mt-6">
 
     <input 
-        type="text" name="pesquisar"
+        type="text" 
+
+        name="pesquisar"
 
         class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full"
 
@@ -14,28 +16,14 @@
 
 <section class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
 
-    <?php foreach($livros as $livro): ?>
+    <?php 
+        
+        foreach($livros as $livro) {
 
-        <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+            require 'partials/_livro.php';
 
-            <div class="flex">
-
-                <div class="w-1/3">Imagem</div>
-
-                <div class="space-y-1">
-
-                    <a href="/livro?id=<?= $livro->id ?>" class="font-semibold hover:underline"><?= $livro->titulo ?></a>
-                    <div class="text-xs italic"><?= $livro->autor ?></div>
-                    <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliações)</div>
-
-                </div>
-
-            </div>
-
-            <div class="text-sm mt-2"><?= $livro->descricao ?></div>
-
-        </div>
-
-    <?php endforeach; ?>
+        } 
+    
+    ?>
 
 </section>
